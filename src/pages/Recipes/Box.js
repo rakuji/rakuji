6910faa90c9333b2453f828db2d2ad9edde943c8
redalygames <67671868+redalygames@users.noreply.github.com>
2022-05-18 17:03:34 +0800

    0518

--- src/pages/Recipes/Box.js
+++ src/pages/Recipes/Box.js
@@ -66,9 +66,14 @@ const Box = () => {
               <div className="col-5 d-flex text-center  align-items-end">
                 <div style={{ margin: "0px -2px" }} className="col-4 bg-body">
                   <div className="p-3 bg-light rounded-top">
-                    <canvas id="canvas" width="16" height="16">
-
-                    </canvas>
+                    <svg width="100" height="70">
+                      <circle
+                        cx="50"
+                        cy="35"
+                        r="30"
+                        style={{ stroke: "black", fill: "white" }}
+                      />
+                    </svg>
                   </div>
                   <div className="p-3 bg-body rounded-bottom border-2 border-light border-end border-bottom border-start">
                     <h7>0</h7>
@@ -76,18 +81,19 @@ const Box = () => {
                     <h7>分鐘</h7>
                   </div>
                 </div>
-                <div style={{ margin: "0px 0px" }} className="col-4 bg-body">
+                <div style={{ margin: "0px 0px" }} className="col-4">
                   <div className="p-3 bg-light rounded-top">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-clock"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                    <svg width="100" height="70">
+                      <circle
+                        cx="50"
+                        cy="35"
+                        r="30"
+                        style={{ stroke: "black", fill: "white" }}
+                      />
+                      <polyline
+                        points="20,20 40,25 60,40 80,120 120,140 200,180"
+                        style={{fill:"red",stroke:"3"}}
+                      />
                     </svg>
                   </div>
                   <div className="p-3 bg-body rounded-bottom border-2 border-light border-end border-bottom border-start">
@@ -98,16 +104,13 @@ const Box = () => {
                 </div>
                 <div style={{ margin: "0px 0px" }} className="col-4 bg-body  ">
                   <div className="p-3 bg-light rounded-top">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-clock"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                    <svg width="100" height="70">
+                      <circle
+                        cx="50"
+                        cy="35"
+                        r="30"
+                        style={{ stroke: "black", fill: "white" }}
+                      />
                     </svg>
                   </div>
                   <div className="p-3 bg-body rounded-bottom border-2 border-light border-end border-bottom border-start">
