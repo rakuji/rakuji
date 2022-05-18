fc14da2dc613363bb79f79a2faa0c16c9807ae12
redalygames <67671868+redalygames@users.noreply.github.com>
2022-05-18 16:35:55 +0800

    0518

--- src/pages/Recipes/Box.js
+++ src/pages/Recipes/Box.js
@@ -28,24 +28,24 @@ const Box = () => {
                     <a href="#" className="p-0">
                       <i
                         style={{ padding: "5px" }}
-                        className="fa-brands fa-facebook"
+                        className="fa-brands fa-facebook "
                       ></i>
                     </a>
                     <a href="#" className="p-0">
                       <i
-                        style={{ padding: "5px" }}
+                        style={{ padding: "5px", color: "red" }}
                         className="fa-brands fa-instagram recipesIg"
                       ></i>
                     </a>
                     <a href="#" className="p-0">
                       <i
-                        style={{ padding: "5px" }}
+                        style={{ padding: "5px", color: "green" }}
                         className="fa-brands fa-line recipesLine"
                       ></i>
                     </a>
                     <a href="#" className="p-0">
                       <i
-                        style={{ padding: "5px" }}
+                        style={{ padding: "5px", color: "red" }}
                         className="fa-brands fa-youtube recipesYoutube"
                       ></i>
                     </a>
@@ -66,17 +66,9 @@ const Box = () => {
               <div className="col-5 d-flex text-center  align-items-end">
                 <div style={{ margin: "0px -2px" }} className="col-4 bg-body">
                   <div className="p-3 bg-light rounded-top">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-clock"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-                    </svg>
+                    <canvas id="canvas" width="16" height="16">
+
+                    </canvas>
                   </div>
                   <div className="p-3 bg-body rounded-bottom border-2 border-light border-end border-bottom border-start">
                     <h7>0</h7>
