8405db05e6c940c25cbc18ed62b6aa56daff2bb1
RaisenXiao <101316746+RaisenXiao@users.noreply.github.com>
2022-05-20 10:57:50 +0800

    5/20

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,30 +2,45 @@ import React, { Component } from "react";
 import { Form, Col, Button } from "react-bootstrap";
 import "./Contact.css";
 
-
 const Contact = () => {
   return (
     // <div style={{ minHeight: " calc(100vh - 86px - 308px)" }}>關於我們</div>
-    <div className="c1">
-      <div className="content">
-        <h2>聯絡我們</h2>
-        <h3>請填寫您的聯絡方式</h3>
-        <div className="g1">
+    <div className="container shadow p-3 mb-5 bg-white rounded d-grid gap-3 my-5">
+      <div className="row justify-content-center">
+        <div className="col">
+          <h2>聯絡我們</h2>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <h3>請填寫您的聯絡方式</h3>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>姓名*</Form.Label>
+            <Form.Label>姓名</Form.Label>
             <Form.Control type="text" placeholder="" />
           </Form.Group>
         </div>
-        <div className="g2">
+      </div>
+      <div className="row">
+        <div className="col">
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" placeholder="" />
           </Form.Group>
         </div>
-        <div className="g3">
+      </div>
+      <div className="row">
+        <div className="col">
           <div>選擇項目</div>
         </div>
-        <div className="g4">
+      </div>
+      <div className="row">
+        <div className="col">
           <Form.Select aria-label="Default select example">
             <option>一般消費建議</option>
             <option value="1">One</option>
@@ -33,20 +48,26 @@ const Contact = () => {
             <option value="3">Three</option>
           </Form.Select>
         </div>
-        <div className="g5">
+      </div>
+      <div className="row">
+        <div className="col">
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
             <Form.Label>主旨</Form.Label>
             <Form.Control type="text" placeholder="" />
           </Form.Group>
         </div>
-        <div className="g6">
+      </div>
+      <div className="row">
+        <div className="col">
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>留言</Form.Label>
             <Form.Control as="textarea" rows={3} />
           </Form.Group>
         </div>
-        <div className="g7">
-        <input type ="button" value="送出" className="btn1"></input>
+      </div>
+      <div className="row">
+        <div className="col d-flex justify-content-center">
+          <input type="button" value="送出" className="btn1"></input>
         </div>
       </div>
     </div>
