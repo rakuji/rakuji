11a7ce058d68996061684998afac276d1b5ffd16
RaisenXiao <101316746+RaisenXiao@users.noreply.github.com>
2022-05-23 01:04:47 +0800

    add caroussel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,37 @@
 import React from "react";
-import { Form, Col, Button, Row, Container, Image } from "react-bootstrap";
+import {
+  Form,
+  Col,
+  Button,
+  Row,
+  Container,
+  Image,
+  Carousel,
+} from "react-bootstrap";
 import "./Home.css";
+import pic01 from "./pic/pic01.png";
 
 const Home = () => {
   return (
- <Container>
-   <Row></Row>
-   <Row></Row>
-   <Row></Row>
-   <Row></Row>
-   <Row></Row>
- </Container>
+    <Container>
+      <Row>
+        <Carousel nextIcon  aria-hidden="true" >
+          <Carousel.Item >
+            <img className="d-block w-100"  src={pic01} alt="pic01" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic01} alt="pic01" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic01} alt="pic01" />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row></Row>
+      <Row></Row>
+      <Row></Row>
+      <Row></Row>
+    </Container>
   );
 };
 
