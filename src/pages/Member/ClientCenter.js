f25f9c98479d58f6e226d4933c28be8dca2560ca
linjoy017 <100506821+linjoy017@users.noreply.github.com>
2022-05-23 16:12:01 +0800

    avatar-center

--- src/pages/Member/ClientCenter.js
+++ src/pages/Member/ClientCenter.js
@@ -7,8 +7,9 @@ function ClientCenter() {
     return (
 
         <div className="my-login-page">
-            <header>header</header>
+            <div>header</div>
             <nav aria-label="breadcrumb">
+
                 <ol className="breadcrumb">
                     <li className="breadcrumb-item"><a href="#index">首頁</a></li>
                     <li className="breadcrumb-item"><a href="#">會員管理</a></li>
@@ -17,61 +18,39 @@ function ClientCenter() {
             </nav>
             <main className="d-flex justify-content-center">
                 <aside className="m-3">
-                    <div className="form-group ">
+                    <div className="form-group">
+                        <div className="card" style={{ width: "18rem" }}>
 
-                        <div className="card" style={{width: "18rem"}}>
-
-                            <div className="m-3 pl-5">
-
-                                <div className="circle box ">
-
-
-                                    <img src="/src/image/account/account-1.jpg" className=" p-control" alt="..." />
-
+                            {/* 大頭照 */}
+                            <div className="d-flex">
+                                <div className="justify-content-center text-center ">
+                                    123
+                                    <div className="avatar">123</div>
                                 </div>
-
                             </div>
-
-                            <div className="card-body ">
-
-                                <div className="justify-content-center text-sm-center m-3">
+                            <div className="card-body">
+                                <div className="text-sm-center m-3">
                                     <p className="card-text">Coupons: 2張</p>
                                 </div>
                             </div>
 
 
                             <ul className="list-group list-group-flush">
-
-                                {/* <li className="list-group-item m-3">
-                                    <div className="mb-2">
-                                        <span className="text-control ">日期</span>
-                                    </div>
-                                    <div className=" pt-2">
-                                        <img src="/src/image/account/Date.png" alt="" />
-                                        <br />
-                                    </div>
-                                </li> */}
-
                                 <li className="list-group-item m-3">
                                     <span className="text-control">您的餐點喜好</span>
                                     <div className="ml-4 pt-2">
-                                        <img src="/src/image/account/A.png" alt="" />
+
+                                        <img src={require("../../images/account/A.png")} />
                                         黃金豬排丼飯<br />
-                                        <img src="/src/image/account/B.png" alt="" />
+                                        <img src={require("../../images/account/B.png")} />
                                         可樂餅<br />
-                                        <img src="/src/image/account/C.png" alt="" />
+                                        <img src={require("../../images/account/C.png")} />
                                         烤鮭魚蘿蔔味增湯<br />
                                     </div>
                                 </li>
-                            </ul>
-                        </div>
-                    </div>
-                </aside>
-                <article>
-                    <div className="card fat">
-                        <div className="card-body">
 
-                            <nav className="nav nav-pills flex-column flex-sm-row">
+                            </ul>
+                            <nav className="nav nav-pills flex-column flex-sm-column">
                                 <a className="flex-sm-fill text-sm-center nav-link active" aria-current="page"
                                     href="./client-center.html">會員個人資料</a>
                                 <a className="flex-sm-fill text-sm-center nav-link" href="./order-list.html"
@@ -80,6 +59,13 @@ function ClientCenter() {
                                     target="_self">優惠資料紀錄</a>
 
                             </nav>
+                        </div>
+                    </div>
+                </aside>
+                <article className="m-3">
+                    <div className="card fat">
+                        <div className="card-body">
+
                             <br />
 
                             <form method="POST" className="my-login-validation" noValidate="">
@@ -102,22 +88,22 @@ function ClientCenter() {
 
                                 <div className="form-group">
                                     <label htmlFor="">出生年月日</label>
-                                    <input  type="text" className="form-control" name="" value="1983-03-10" disabled />
+                                    <input type="text" className="form-control" name="" value="1983-03-10" disabled />
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="">居住縣市</label>
-                                    <input  type="text" className="form-control" name="" value="彰化縣" disabled />
+                                    <input type="text" className="form-control" name="" value="彰化縣" disabled />
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="">居住地址</label>
-                                    <input  type="text" className="form-control" name="" value="伸港鄉埤墘一路13號" disabled />
+                                    <input type="text" className="form-control" name="" value="伸港鄉埤墘一路13號" disabled />
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="">子嗣</label>
-                                    <input  type="text" className="form-control" name="" value="有小孩" disabled />
+                                    <input type="text" className="form-control" name="" value="有小孩" disabled />
 
                                 </div>
 
