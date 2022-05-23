e33fc4dc8d20dd6b3f6870b45272714819682d63
RaisenXiao <101316746+RaisenXiao@users.noreply.github.com>
2022-05-23 11:27:56 +0800

    add home1.0

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,15 +9,21 @@ import {
   Carousel,
 } from "react-bootstrap";
 import "./Home.css";
+import { Link } from "react-router-dom";
+
 import pic01 from "./pic/pic01.png";
+import pic02 from "./pic/pic02.png";
+import pic03 from "./pic/pic03.png";
+import pic04 from "./pic/pic04.png";
+import pic05 from "./pic/pic05.png";
 
 const Home = () => {
   return (
     <Container>
       <Row>
-        <Carousel nextIcon  aria-hidden="true" >
-          <Carousel.Item >
-            <img className="d-block w-100"  src={pic01} alt="pic01" />
+        <Carousel nextIcon aria-hidden="true">
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic01} alt="pic01" />
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src={pic01} alt="pic01" />
@@ -27,10 +33,34 @@ const Home = () => {
           </Carousel.Item>
         </Carousel>
       </Row>
-      <Row></Row>
-      <Row></Row>
-      <Row></Row>
-      <Row></Row>
+      <Row>
+        <Carousel nextIcon aria-hidden="true">
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic02} alt="pic02" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic02} alt="pic02" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic02} alt="pic02" />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row>
+        <Link to="products">
+          <Image className="d-block w-100" src={pic03} alt="pic03"></Image>
+        </Link>
+      </Row>
+      <Row>
+        <Link to="/recipes">
+          <Image className="d-block w-100" src={pic04} alt="pic04"></Image>
+        </Link>
+      </Row>
+      <Row>
+        <Link to="/about">
+          <Image className="d-block w-100" src={pic05} alt="pic05"></Image>
+        </Link>
+      </Row>
     </Container>
   );
 };
