b95e85abbab5c6508eb21a8c5dcc285fe2e1a4c3
LiangYuChen <102126452+Liang-Yu-Chen@users.noreply.github.com>
2022-05-24 16:44:47 +0800

    524-4

--- src/pages/Newslist/Cooperationform.js
+++ src/pages/Newslist/Cooperationform.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React ,{useState} from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Cooperationform.css";
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import InputGroup from 'react-bootstrap/InputGroup'
 // pages
 
 import Breadcrumb from "./Breadcrumb";
 
-const Cooperationform = () => {
+import titleP from "./titleP.png";
+
+
+function  Cooperationform  ()  {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
-    <main>
+    <>
       <img
         src={require("./002.png")}
         width="100%"
@@ -17,45 +33,51 @@ const Cooperationform = () => {
         className="d-block  mx-auto"
         alt="..."
       />
-      {/* test */}
+  
 
-      {/* test */}
       {/* 麵包屑製作問題 */}
 
       <Breadcrumb />
       <div className="container">
         <div className="col"></div>
-      </div>
-
-      <Form style={{}} className="container   , block">
-        <Form.Label className="client fs-1" src="">
-          商家聯絡資料
-        </Form.Label>
+     
+      <Form style={{}} className="container , block">
+        <img className="sixyellow" src={titleP} alt="" />
+        <Form.Label className="client fs-1">商家聯絡資料</Form.Label>
         <br></br>
         <Form.Label className="fs-6">2022/12/56 - 2022/12/56</Form.Label>
         <br></br>
         <Form.Label className="fs-4">合作主旨</Form.Label>
         {/* radio 其他這邊待修正 */}
-        <div>
-          <input type="radio" value="1" />
-          異業合作
-        </div>
-        <div>
-          <input type="radio" value="2" />
-          廣告/宣傳
-        </div>{" "}
-        <div>
-          <input type="radio" value="3" />
-          分店拓展
-        </div>{" "}
-        <div>
-          <input type="radio" value="4" />
-          其他請簡述
-          <Form.Group className="mb-3," controlId="">
-            <Form.Label></Form.Label>
-            <Form.Control type="" placeholder="" />
-          </Form.Group>
-        </div>
+
+        <Form.Check
+          type="radio"
+          label="異業合作"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios1"
+        />
+        <Form.Check
+          type="radio"
+          label="廣告/宣傳"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios2"
+        />
+        <Form.Check
+          type="radio"
+          label="分店拓展"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios3"
+        />
+        <Form.Check
+          type="radio"
+          label="其他"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios3"
+        />
+          {/* 打CODE 設定表單 */}
+    
+          <Form.Label></Form.Label>
+
         <Form.Group className="mb-3" controlId="">
           <Form.Label>公司單位/名稱</Form.Label>
           <Form.Control type="" placeholder="" />
@@ -91,7 +113,105 @@ const Cooperationform = () => {
         icon回上一頁
       </button>
       <br></br>
-    </main>
+      </div>  
+
+      <div>TEST</div>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>公司單位/名稱</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="資展國際股份有限公司"
+            defaultValue=""
+          />
+          <Form.Control.Feedback></Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>聯絡人姓名</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="王大雄 "
+            defaultValue=""
+          />
+          <Form.Control.Feedback></Form.Control.Feedback>         
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom03">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            required
+            type="Email"
+            placeholder="xxxxxx@gmail.com "
+            defaultValue=""
+          />
+             <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback></Form.Control.Feedback>         
+        </Form.Group>
+
+
+
+
+
+
+
+
+
+
+
+        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+          <Form.Label>Username</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom04">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="City" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom05">
+          <Form.Label>State</Form.Label>
+          <Form.Control type="text" placeholder="State" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid state.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom06">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control type="text" placeholder="Zip" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid zip.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Form.Group className="mb-3">
+        <Form.Check
+          required
+          label="Agree to terms and conditions"
+          feedback="You must agree before submitting."
+          feedbackType="invalid"
+        />
+      </Form.Group>
+      <Button type="submit">Submit form</Button>
+    </Form>
+    </>
   );
 };
 
--- src/pages/Newslist/Votelist.js
+++ src/pages/Newslist/Votelist.js
@@ -30,18 +30,18 @@ const Votelist = () => {
               《料理之王》火熱開打，選手大展身手，做出一道道美味佳餚，滿足視覺、嗅覺、味覺，征服你的感官體驗！看完節目還念念不忘？想成為和選手一樣的料理達人？現在就上網票選節目中你最愛的料理食譜，讓你最愛的料理被大家看見、獲得百萬曝光機會，成為家喻戶曉的全民料理！力挺你心儀的菜色，還有機會抽中全家禮券，快來為你喜歡的美食投票吧!
             </div>
             <br></br>
-            {/* li padding 多出來 */}
+            
             <li className="list-group list-group-flush">
-              <li className="list-group-item fs-5 ">
+              <li className="list-group-item fs-5 p-0 ">
                 【活動辦法】 每投票一次即可獲得一次抽獎資格!
                 每人每天最多可投兩票，投票越多次中獎機會越高！
               </li>
-              <li className="list-group-item fs-5">
+              <li className="list-group-item fs-5 p-0">
                 【活動獎項】 壹獎：Drinkmate氣泡機[市價$2,980元]，共3名。
                 貳獎：全家禮物卡500元，共10名。 參獎：全家禮物卡300元，共10名。
                 普獎：全家禮物卡100元，共100名。
               </li>
-              <li className="list-group-item fs-5 ">
+              <li className="list-group-item fs-5 p-0">
                 【注意事項】
                 活動時間：西元（下同）2022年06月01日15:00起至2022年06月30日12:00止。
                 活動結果公佈：2022年07月18日前於活動頁公佈得獎者。
