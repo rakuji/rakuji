478f68605ff2bbbdb60616e2b077557d935f44d6
RaisenXiao <101316746+RaisenXiao@users.noreply.github.com>
2022-05-25 16:11:05 +0800

    add state

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,74 +1,115 @@
 import React from "react";
-import { Form, Col, Row, Container } from "react-bootstrap";
+import { Form, Col, Row, Container, Select } from "react-bootstrap";
 import "./Contact.css";
 
-const Contact = () => {
-  return (
-    <Container className="shadow p-3 mb-5 bg-white rounded d-grid gap-3 my-5 px-5">
-      <Row className="justify-content-center">
-        <Col>
-          <h2>聯絡我們</h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h3>請填寫您的聯絡方式</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>姓名</Form.Label>
-            <Form.Control type="text" placeholder="" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div>選擇項目</div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Select aria-label="Default select example">
-            <option>一般消費建議</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </Form.Select>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-            <Form.Label>主旨</Form.Label>
-            <Form.Control type="text" placeholder="" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>留言</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex justify-content-center">
-          <input type="button" value="送出" className="btn1"></input>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      select: "",
+      subject: "",
+      message: ""
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const name = event.target.name;
+    const type = event.target.type;
+    const value =
+      type === "checkbox" ? event.target.checked : event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Container className="shadow p-3 mb-5 bg-white rounded d-grid gap-3 my-5 px-5">
+        <Form onSubmit={this.handleSubmit}>
+          <Row className="justify-content-center">
+            <Col>
+              <h2>聯絡我們</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3>請填寫您的聯絡方式</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <label htmlfor="name">姓名</label>
+                <input type="name" className="form-control" id="name" name="name" placeholder="姓名" value={this.state.name} onChange={this.handleInputChange}></input>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput2"
+              >
+                <Form.Label>Email</Form.Label>
+                <input type="email" className="form-control" id="email" name="email" placeholder="xxxxxx@com" value={this.state.email} onChange={this.handleInputChange}></input>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div>選擇項目</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Select type="select" id="select" name="select" placeholder="" value={this.state.select} onChange={this.handleInputChange}>
+                <option value="一般消費建議">一般消費建議</option>
+                <option value="餐點回饋">餐點回饋</option>
+                <option value="客訴">客訴</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput3"
+              >
+                <Form.Label>主旨</Form.Label>
+                <input type="subject" className="form-control" id="subject" name="subject" placeholder="主旨" value={this.state.subject} onChange={this.handleInputChange}></input>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>留言</Form.Label>
+                <textarea type="meessage" className="form-control" id="message" name="message" placeholder="內文" value={this.state.message} onChange={this.handleInputChange}></textarea>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="d-flex justify-content-center">
+              <input type="submit" value="送出" className="btn1"></input>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    );
+  }
+}
 
 export default Contact;
