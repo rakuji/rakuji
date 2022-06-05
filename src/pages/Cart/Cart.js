d07d5795dc00a4dcf294441594bdb1151cfc18cf
hsuan21 <yoyoman870221@gmail.com>
2022-06-06 03:53:47 +0800

    0606

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,143 +1,355 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
-import CartItemArea from './components/CartItemArea';
-import $ from "jquery"
-import { Modal, Button } from 'react-bootstrap'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import CartItemArea from "./components/CartItemArea";
+import $ from "jquery";
+import { Modal, Button } from "react-bootstrap";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { LuckyWheel, LuckyGrid } from "@lucky-canvas/react";
 
-
-import { useCart } from './utils/useCart'
-
+import { useCart } from "./utils/useCart";
 
 const Cart = (props) => {
+  const [coupon, setCoupon] = useState(0);
+  const [couponPrice, setCouponPrice] = useState(0);
+  const [couponMsg, setCouponMsg] = useState("");
 
-    const [coupon, setCoupon] = useState(0)
-    const [couponPrice, setCouponPrice] = useState(0)
-    const [couponMsg, setCouponMsg] = useState("")
-
-
-    const couponhandler = () => {
-
-        switch (coupon) {
-            case "VIP666":
-                setCouponMsg("成功套用優惠碼，已折抵100元!")
-                setCouponPrice(100)
-                // handleShow()
-                Swal.fire({
-                    icon: 'success',
-                    title: '成功套用優惠碼，已折抵100元!',
-                })
-                break;
-            case "HAPPY888":
-                setCouponMsg("成功套用優惠碼，已折抵50元!")
-                setCouponPrice(50)
-                Swal.fire({
-                    icon: 'success',
-                    title: '成功套用優惠碼，已折抵50元!',
-                })
-                break;
-            case "RAKUJIISGOOD":
-                setCouponMsg("成功套用優惠碼，已折抵30元!")
-                setCouponPrice(30)
-                Swal.fire({
-                    icon: 'success',
-                    title: '成功套用優惠碼，已折抵30元!',
-                })
-                break;
-            default:
-                setCouponMsg("請輸入正確的優惠碼!")
-                setCouponPrice(0)
-                Swal.fire({
-                    icon: 'warning',
-                    title: '請輸入正確的優惠碼!',
-                })
-        }
+  const couponhandler = () => {
+    switch (coupon) {
+      case "VIP666":
+        setCouponMsg("成功套用優惠碼，已折抵100元!");
+        setCouponPrice(100);
+        // handleShow()
+        Swal.fire({
+          icon: "success",
+          title: "成功套用優惠碼，已折抵100元!",
+        });
+        break;
+      case "HAPPY888":
+        setCouponMsg("成功套用優惠碼，已折抵50元!");
+        setCouponPrice(50);
+        Swal.fire({
+          icon: "success",
+          title: "成功套用優惠碼，已折抵50元!",
+        });
+        break;
+      case "RAKUJIISGOOD":
+        setCouponMsg("成功套用優惠碼，已折抵30元!");
+        setCouponPrice(30);
+        Swal.fire({
+          icon: "success",
+          title: "成功套用優惠碼，已折抵30元!",
+        });
+        break;
+      case "SEEYOUAGAIN":
+        setCouponMsg("成功套用優惠碼，已折抵10元!");
+        setCouponPrice(10);
+        Swal.fire({
+          icon: "success",
+          title: "成功套用優惠碼，已折抵10元!",
+        });
+        break;
+      default:
+        setCouponMsg("請輸入正確的優惠碼!");
+        setCouponPrice(0);
+        Swal.fire({
+          icon: "warning",
+          title: "請輸入正確的優惠碼!",
+        });
     }
+  };
 
-    localStorage.setItem("couponPrice", couponPrice)
+  localStorage.setItem("couponPrice", couponPrice);
 
+  // 使用hooks 解出所需的狀態與函式(自context)
+  const { cart, items, plusOne, minusOne, removeItem } = useCart();
 
-    // 使用hooks 解出所需的狀態與函式(自context)
-    const { cart, items, plusOne, minusOne, removeItem } = useCart()
+  console.log(cart);
 
-    console.log(cart)
+  // --------------------------------------------------------------------------
 
+  const [blocks] = useState([{ padding: "10px", background: "#489AD5" }]);
+  const [prizes] = useState([
+    {
+      background: "#C6E7F4",
+      fonts: [
+        {
+          text: "折抵100元",
+          top: "40px",
+          fontWeight: "600",
+          fontStyle: "Arial, Helvetica, sans-serif",
+        },
+      ],
+    },
+    {
+      background: "#58C2DD",
+      fonts: [
+        {
+          text: "再接再厲",
+          top: "40px",
+          fontWeight: "600",
+          fontStyle: "Arial, Helvetica, sans-serif",
+        },
+      ],
+    },
+    {
+      background: "#C6E7F4",
+      fonts: [
+        {
+          text: "折抵50元",
+          top: "40px",
+          fontWeight: "600",
+          fontStyle: "Arial, Helvetica, sans-serif",
+        },
+      ],
+    },
+    {
+      background: "#58C2DD",
+      fonts: [
+        {
+          text: "下次再來",
+          top: "40px",
+          fontWeight: "600",
+          fontStyle: "Arial, Helvetica, sans-serif",
+        },
+      ],
+    },
+    {
+      background: "#C6E7F4",
+      fonts: [
+        {
+          text: "折抵30元",
+          top: "40px",
+          fontWeight: "600",
+          fontStyle: "Arial, Helvetica, sans-serif",
+        },
+      ],
+    },
+    {
+      background: "#58C2DD",
+      fonts: [
+        {
+          text: "銘謝惠顧",
+          top: "40px",
+          fontWeight: "600",
+          fontStyle: "Arial, Helvetica, sans-serif",
+        },
+      ],
+    },
+  ]);
+  const [buttons] = useState([
+    { radius: "40%", background: "#489AD5" },
+    { radius: "35%", background: "#C6E7F4" },
+    {
+      radius: "30%",
+      background: "#BE5438",
+      pointer: true,
+      fonts: [
+        { text: "抽獎", top: "-10px", fontColor: "#fff", fontWeight: "600" },
+      ],
+    },
+  ]);
 
-    // 對話盒使用
-    const [show, setShow] = useState(false)
+  const myLucky = useRef();
 
+  // --------------------------------------------------------------------------
 
-    const handleClose = () => setShow(false)
-    const handleShow = () => setShow(true)
+  // 對話盒使用
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const messageModal = (
-        <Modal show={show} onHide={handleClose} keyboard={false}>
-            <Modal.Header closeButton>
-                <Modal.Title>套用優惠碼訊息</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>{couponMsg}</Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    確定
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    )
+  const messageModal = (
+    <Modal show={show} onHide={handleClose} keyboard={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>套用優惠碼訊息</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{couponMsg}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          確定
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 
+  // --------------------------------------------------------------------------
+  const [modalShow, setModalShow] = React.useState(false);
+
+  function MyVerticallyCenteredModal(props) {
     return (
-        <div className='container' style={{ minHeight: " calc(100vh - 86px - 308px)" }}>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            輪盤小遊戲
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="d-flex justify-content-center LuckyWheelFont">
+            點選抽獎就有機會獲得優惠碼!
+          </p>
+          <div className="d-flex justify-content-center">
+            {/* 輪盤抽獎 */}
+            <LuckyWheel
+              ref={myLucky}
+              width="400px"
+              height="400px"
+              blocks={blocks}
+              prizes={prizes}
+              buttons={buttons}
+              onStart={() => {
+                myLucky.current.play();
+                setTimeout(() => {
+                  const index = (Math.random() * 6) >> 0;
+                  myLucky.current.stop(index);
+                }, 2500);
+              }}
+              onEnd={(prize) => {
+                // alert("恭喜你抽到 " + prize.fonts[0].text + " 獎");
+                Swal.fire({
+                  icon: "warning",
+                  title: prize.fonts[0].text,
+                });
 
+                switch (prize.fonts[0].text) {
+                  case "折抵100元":
+                    // handleShow()
+                    Swal.fire({
+                      icon: "success",
+                      title: "恭喜你獲得折抵100元優惠碼!",
+                    });
+                    break;
+                  case "再接再厲":
+                    Swal.fire({
+                      icon: "success",
+                      title: "再接再厲",
+                      text:"別難過，給你安慰獎"
+                    });
+                    break;
+                  case "折抵50元":
+                    Swal.fire({
+                      icon: "success",
+                      title: "恭喜你獲得折抵50元優惠碼!",
+                    });
+                    break;
+                  case "下來再來":
+                    Swal.fire({
+                      icon: "success",
+                      title: "下來再來",
+                      text:"別難過，給你安慰獎"
+                    });
+                    break;
 
-            <h3>購物車</h3>
-            <CartItemArea />
+                  case "折抵30元":
+                    Swal.fire({
+                      icon: "success",
+                      title: "恭喜你獲得折抵30元優惠碼!",
+                    });
+                    break;
+                    
+                  case "銘謝惠顧":
+                    Swal.fire({
+                      icon: "success",
+                      title: "銘謝惠顧",
+                      text:"別難過，給你安慰獎"
+                    });
+                    break;
+                }
+              }}
+            />
+          </div>
+        </Modal.Body>
+        {/* <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer> */}
+      </Modal>
+    );
+  }
+  // --------------------------------------------------------------------------
 
-            <div className="row justify-content-end">
+  return (
+    <div
+      className="container"
+      style={{ minHeight: " calc(100vh - 86px - 308px)" }}
+    >
+      <h3>購物車</h3>
+      <CartItemArea />
 
-                <div className="col-3 cart_checkarea me-5">
+      <div className="row justify-content-end">
+        <div className="col-3 cart_checkarea me-5">
+          <form className="input-group mb-3">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="請輸入優惠碼"
+              aria-label="Recipient's username"
+              aria-describedby="button-coupon"
+              required=""
+              onChange={(e) => {
+                setCoupon(e.target.value);
+              }}
+            />
+            <button
+              type="button"
+              className="btn btn-right fz-0"
+              id="button-coupon"
+              onClick={couponhandler}
+            >
+              套用優惠碼
+            </button>
+          </form>
 
-                    <form className="input-group mb-3">
-                        <input className="form-control" type="text" placeholder="請輸入優惠碼" aria-label="Recipient's username" aria-describedby="button-coupon" required="" onChange={(e) => {
-                            setCoupon(e.target.value)
-                        }} />
-                        <button type="button" className="btn btn-right fz-0" id="button-coupon" onClick={couponhandler}> 套用優惠碼 </button>
-                    </form>
+          <div className="quantity my-3">
+            <p>數量</p>
+            <p>共{cart.totalItems}件</p>
+          </div>
 
+          <div className="price my-3">
+            <p>小計</p>
+            <p>${cart.cartTotal}</p>
+          </div>
 
-                    <div className='quantity my-3'>
-                        <p>數量</p>
-                        <p>共{cart.totalItems}件</p>
-                    </div>
+          <div className="price my-3">
+            <p>優惠折扣</p>
+            <p>$-{couponPrice}</p>
+          </div>
 
-                    <div className='price my-3'>
-                        <p>小計</p>
-                        <p>${cart.cartTotal}</p>
-                    </div>
+          <hr />
 
-                    <div className='price my-3'>
-                        <p>優惠折扣</p>
-                        <p>$-{couponPrice}</p>
-                    </div>
+          <div className="price my-3">
+            <p>總計</p>
+            <b className="cart_total_price">
+              $
+              {cart.cartTotal - couponPrice <= 0
+                ? 0
+                : cart.cartTotal - couponPrice}
+            </b>
+          </div>
 
-                    <hr />
+          <Link to="/cart/cart_info">
+            <button className="go_check my-4">前往結帳</button>
+          </Link>
+        </div>
+      </div>
 
-                    <div className='price my-3'>
-                        <p>總計</p>
-                        <b className='cart_total_price'>${(cart.cartTotal - couponPrice <= 0) ? 0 : (cart.cartTotal - couponPrice)}</b>
-                    </div>
+      {messageModal}
 
-                    <Link to="/cart/cart_info">
-                        <button className="go_check my-4">前往結帳</button>
-                    </Link>
+      <div className="coupon_game" onClick={() => setModalShow(true)}>
+        <i class="fa-solid fa-gift"></i>
+      </div>
 
-                </div>
-            </div>
-            {messageModal}
-        </div >
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </div>
+  );
+};
 
-    )
-}
-
-export default Cart
+export default Cart;
--- src/pages/Products/Product_detail.js
+++ src/pages/Products/Product_detail.js
@@ -1,73 +1,67 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
 import { GlassMagnifier } from "react-image-magnifiers";
-import { useCart } from '../Cart/utils/useCart'
-import { Modal, Button } from 'react-bootstrap'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import { useCart } from "../Cart/utils/useCart";
+import { Modal, Button } from "react-bootstrap";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
 
 // import products from './data/products.json'
 
 const Product_detail = (props) => {
-
   // 從資料庫取得資料
-  const [datas, setDatas] = useState([])
+  const [datas, setDatas] = useState([]);
 
   const fetchData = async () => {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/products`);
     const results = await response.json();
     setDatas(results);
     // console.log(results)
-
-  }
+  };
   useEffect(() => {
     fetchData();
-  }, [])
-
+  }, []);
 
   // 取得:id資料
   const [product, setProduct] = useState({
     id: 0,
-    picture: 'https://i.imgur.com/1GrakTl.jpg',
-    name: '',
+    picture: "https://i.imgur.com/1GrakTl.jpg",
+    name: "",
     price: 0,
-    categroy: '',
-    introduction: ''
-  })
+    categroy: "",
+    introduction: "",
+  });
 
   //取得url productId
-  const params = useParams()
-
+  const params = useParams();
 
   useEffect(() => {
     // 取得json檔
-    const item = datas.find((v) => String(v.id) === params.productId)
+    const item = datas.find((v) => String(v.id) === params.productId);
 
     if (item) {
-      setProduct(item)
+      setProduct(item);
     }
-  }, [datas])
-
-
+  }, [datas]);
 
   //商品數量
-  const [count, setCount] = useState(1)
-
+  const [count, setCount] = useState(1);
 
   // 對話盒使用
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
   // 對話盒中的商品名稱
-  const [productName, setProductName] = useState('')
+  const [productName, setProductName] = useState("");
 
-  const { addItem } = useCart()
+  const { addItem } = useCart();
 
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const showModal = (name) => {
-    setProductName('產品：' + name + '已成功加入購物車')
-    handleShow()
-  }
+    setProductName("產品：" + name + "已成功加入購物車");
+    handleShow();
+  };
 
   const messageModal = (
     <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
@@ -83,88 +77,100 @@ const Product_detail = (props) => {
           variant="primary"
           onClick={() => {
             // 導向購物車頁面
-            props.history.push('/cart')
+            props.history.push("/cart");
           }}
         >
           前往購物車結帳
         </Button>
       </Modal.Footer>
     </Modal>
-  )
+  );
 
+ 
 
   return (
-    <div className='container product_detail pb-5'>
+    <div className="container-xxl product_detail pb-5">
       <div className="row">
-
         {/* 商品圖片 */}
         <div className="col-6 product_img d-flex justify-content-center align-items-center">
-
-
-          <div className='product_img_container'>
-            <img src={`/img/products/${product.picture}`} alt="" />
+          <div className="product_img_container">
+            <img
+              className="img-fluid"
+              src={`/img/products/${product.picture}`}
+              alt=""
+            />
           </div>
-
-          {/* <GlassMagnifier
-            className='product_img'
-            imageSrc={`/img/products/${product.picture}`}
-            imageAlt="Example"
-            largeImageSrc={`/img/products/${product.picture}`} // Optional
-            magnifierSize="35%"
-            magnifierBorderSize={5}
-            magnifierBorderColor="rgba(255, 255, 255, .5)"
-          /> */}
-
         </div>
 
         {/* 商品資訊 */}
         <div className="col-6 product_info">
-          <h1 className='pb-3'>{product.name}</h1>
-          <p className='pb-5'>{product.introduction}</p>
-          <h1 className='pb-5'>NT{product.price}</h1>
+          <h1 className="pb-3">{product.name}</h1>
+          <p className="pb-5">{product.introduction}</p>
+          <h1 className="pb-5">NT{product.price}</h1>
 
           <div className="input-group mb-4 product_counts">
-            <button className="btn btn-primary" type="button" onClick={() => {
-              if (count >= 2) {
-                setCount(count - 1)
-              }
-            }}>
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={() => {
+                if (count >= 2) {
+                  setCount(count - 1);
+                }
+              }}
+            >
               <i className="fa-solid fa-minus"></i>
             </button>
 
-            <input type="text" className="form-control text-center" min="1" onChange={e => setCount(parseInt(e.target.value))} value={count} />
+            <input
+              type="text"
+              className="form-control text-center"
+              min="1"
+              onChange={(e) => setCount(parseInt(e.target.value))}
+              value={count}
+            />
 
-            <button className="btn btn-primary rounded-end" type="button" onClick={() => { setCount(count + 1) }}>
+            <button
+              className="btn btn-primary rounded-end"
+              type="button"
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
               <i className="fa-solid fa-plus"></i>
             </button>
 
-            <button className="btn btn-outline-primary d-none d-md-block ms-1 rounded fw-bold border-2 add_cart ms-3" type="button" onClick={() => {
-              console.log(product)
-              // 商品原本無數量屬性(quantity)，要先加上
-              const item = { ...product, quantity: count }
-              console.log(item)
-              // 注意: 重覆加入會自動+1產品數量
-              addItem(item)
-              // 呈現跳出對話盒
-              // showModal(product.name)
-              Swal.fire({
-                icon: 'success',
-                title: '商品已加入購物車',
-                timer: 1500,
-                timerProgressBar: true,
-                showCancelButton: false, // There won't be any cancel button
-                showConfirmButton: false // There won't be any confirm button
-              })
-            }}> 加入購物車 </button>
+            <button
+              className="btn btn-outline-primary d-none d-md-block ms-1 rounded fw-bold border-2 add_cart ms-3"
+              type="button"
+              onClick={() => {
+                console.log(product);
+                // 商品原本無數量屬性(quantity)，要先加上
+                const item = { ...product, quantity: count };
+                console.log(item);
+                // 注意: 重覆加入會自動+1產品數量
+                addItem(item);
+                // 呈現跳出對話盒
+                // showModal(product.name)
+                Swal.fire({
+                  icon: "success",
+                  title: "商品已加入購物車",
+                  timer: 1500,
+                  timerProgressBar: true,
+                  showCancelButton: false, // There won't be any cancel button
+                  showConfirmButton: false, // There won't be any confirm button
+                });
+              }}
+            >
+              {" "}
+              加入購物車{" "}
+            </button>
           </div>
         </div>
       </div>
-
       {messageModal}
 
-
     </div>
-  )
-}
+  );
+};
 
-export default Product_detail
+export default Product_detail;
