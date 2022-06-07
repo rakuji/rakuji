2c98725d3ecdf8da6542c90cf6c12b193e505808
LiangYuChen <102126452+Liang-Yu-Chen@users.noreply.github.com>
2022-06-07 11:57:28 +0800

    0607-1

--- src/pages/Newslist/Cooperationform/Cooperationform.js
+++ src/pages/Newslist/Cooperationform/Cooperationform.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
+import Form  from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "../Cooperationform/Cooperationform.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import InputGroup from "react-bootstrap/InputGroup";
+
+
 // pages
+import "../Cooperationform/Cooperationform.css";
 
 import Breadcrumb from "../Breadcrumb";
 
@@ -46,9 +47,10 @@ function Cooperationform() {
           <Form.Label className="fs-6">2022/12/56 - 2022/12/56</Form.Label>
           <br></br>
           <Form.Label className="fs-4">合作主旨</Form.Label>
-          {/* 想改紅色外框提示 表格存入資料庫 */}
+          {/* 想改紅色外框提示 
+                  表格存入資料庫 */}
 
-          <Form.Check 
+          <Form.Check
             required
             type="radio"
             label="異業合作"
@@ -76,22 +78,31 @@ function Cooperationform() {
           {/* 打CODE 設定表單 */}
 
           <Form.Label></Form.Label>
+          
 
-          <Form.Group  hasValidation className="mb-3" controlId="validationCustom02">
+          <Form.Group
+            hasValidation
+            className="mb-3"
+            controlId="validationCustom02"
+          >
             <Form.Label>公司單位/名稱</Form.Label>
-            <Form.Control 
-            type="text" 
-            pattern="^[\u4E00-\u9FA5]{2,14}$"
-            required /> 
+            <Form.Control
+              input-type="text"
+              pattern="^[\u4E00-\u9FA5]{2,14}$"
+              required
+            />
+            <Form.Text className="text-muted">資展國際股份有限公司</Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="validationCustom03">
             <Form.Label>聯絡人姓名</Form.Label>
-            <Form.Control 
-            type="text" 
-            pattern="^[\u4E00-\u9FA5]{2,4}$"
-            required />
+            <Form.Control
+              input-type="text"
+              pattern="^[\u4E00-\u9FA5]{2,4}$"
+              required
+            />
+            <Form.Text className="text-muted">王小銘</Form.Text>
           </Form.Group>
-          <Form.Group   className="mb-3" controlId="validationCustom04">
+          <Form.Group className="mb-3" controlId="validationCustom04">
             <Form.Label>聯絡人手機</Form.Label>
             {/* input-type 設定手機號碼輸入的格式 */}
             <Form.Control
@@ -119,7 +130,7 @@ function Cooperationform() {
               07-2901-234x 請依照正確格式輸入 最後一碼可忽略
             </Form.Text>
           </Form.Group>
-          
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>電子郵件</Form.Label>
             <Form.Control
@@ -128,12 +139,19 @@ function Cooperationform() {
               // 信箱驗證發表要注意 多.也會送出   kcg.gov.tw /gmail.com
               pattern="[a-zA-Z0-9]{3,15}@[a-zA-Z0-9]+\.[a-zA-Z]{2,}+\.[a-zA-Z]"
             />
-            <Form.Text className="text-muted"></Form.Text>
+            <Form.Text className="text-muted">xxxxxx@gmail.com</Form.Text>
           </Form.Group>
 
+          {/* 合作內容字數無法限制 可能是as的原因 */}
           <Form.Group className="mb-3" controlId="">
             <Form.Label>合作內容</Form.Label>
-            <Form.Control type="" placeholder="" as="textarea" rows={6} />
+            <Form.Control
+              type="text"
+              placeholder=""
+              as="textarea"
+              rows={6}
+              required
+            />
           </Form.Group>
 
           <Button variant="primary , d-block , mx-auto " type="submit">
@@ -148,46 +166,32 @@ function Cooperationform() {
       </div>
 
       <div>TEST</div>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+           {/* 想把 icon拿掉 只留下紅色外框提示  validated
+          https://react-bootstrap-v3.netlify.app/components/forms/?
+          表格存入資料庫 */}
+      <Form noValidate validated={validated}  onSubmit={handleSubmit}  >
         <Row className="mb-3">
-          <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Group as={Col} md="4" controlId="validationCustom01" >
             <Form.Label>公司單位/名稱</Form.Label>
             <Form.Control
               required
               type="text"
-              placeholder="資展國際股份有限公司"
-              defaultValue=""
             />
             <Form.Control.Feedback></Form.Control.Feedback>
           </Form.Group>
-    
-          
 
-          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-            <Form.Label>Username</Form.Label>
-            <InputGroup hasValidation>
-              <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                aria-describedby="inputGroupPrepend"
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                Please choose a username.
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
+       
+
+        <Form.Group controlId="formValidationError1" validationState="error">
+        <Form.Label>Input with error</Form.Label>
+    <Form.Control type="text" required/>
+  </Form.Group>
+
         </Row>
-    
-        <Form.Group className="mb-3">
-          <Form.Check
-            required
-            label="Agree to terms and conditions"
-            feedback="You must agree before submitting."
-            feedbackType="invalid"
-          />
-        </Form.Group>
+ 
+
+
+
         <Button type="submit">Submit form</Button>
       </Form>
     </>
