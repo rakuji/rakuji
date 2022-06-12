16325e4b381041d411a5c6a59245c93960342b7a
Liang <jacky1994216@yahoo.com.tw>
2022-06-13 02:03:28 +0800

    0613-1 voteimg

--- src/pages/Newslist/Votelist/Votelist.js
+++ src/pages/Newslist/Votelist/Votelist.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Votelist/Votelist.css";
+import Button from "react-bootstrap/Button";
 
 import Breadcrumb from "../Breadcrumb";
 import MyBackTop from "../../../components/MyBackTop/MyBackTop";
@@ -7,17 +8,17 @@ import MyBackTop from "../../../components/MyBackTop/MyBackTop";
 const Votelist = () => {
   return (
     <main>
-        <img
-          src={require("../image/88.png")}
-          width="100%"
-          height="80%"
-          className="d-block  mx-auto"
-          alt="..."
-        />
-   <div className="tt">
+      <img
+        src={require("../image/88.png")}
+        width="100%"
+        height="80%"
+        className="d-block  mx-auto"
+        alt="..."
+      />
+      <div className="tt">
         <Breadcrumb />
-      </div>  
-      
+      </div>
+
       <div className="container">
         <div className="col">
           <div className="card-body">
@@ -28,7 +29,7 @@ const Votelist = () => {
               《料理之王》火熱開打，選手大展身手，做出一道道美味佳餚，滿足視覺、嗅覺、味覺，征服你的感官體驗！看完節目還念念不忘？想成為和選手一樣的料理達人？現在就上網票選節目中你最愛的料理食譜，讓你最愛的料理被大家看見、獲得百萬曝光機會，成為家喻戶曉的全民料理！力挺你心儀的菜色，還有機會抽中全家禮券，快來為你喜歡的美食投票吧!
             </div>
             <br></br>
-            
+
             <li className="list-group list-group-flush">
               <li className="list-group-item fs-5 p-0 ">
                 【活動辦法】 每投票一次即可獲得一次抽獎資格!
@@ -48,7 +49,7 @@ const Votelist = () => {
           </div>
         </div>
       </div>
-{/* 抓資料庫 */}
+      {/* 抓資料庫 */}
       <div className="container">
         <div className="row row-cols-1 row-cols-md-4 g-4">
           <div className="col_v">
@@ -61,23 +62,29 @@ const Votelist = () => {
               <div className="card-body">
                 <h5 className="card-title">名稱</h5>
                 <br />
-                <button className="card-text fs-6 d-block  mx-auto" alt="...">
+                <Button
+                  className="bu"
+                  variant=" primary , d-block , mx-auto "
+                  type="submit">
                   Like
-                </button>
+                </Button>
               </div>
             </div>
           </div>
-          
-          
+
+
           <div className="col_v">
             <div className="card">
               <img src="..." className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <br />
-                <button className="card-text fs-6 d-block  mx-auto" alt="...">
+                <Button
+                  className="bu"
+                  variant=" primary , d-block , mx-auto "
+                  type="submit">
                   Like
-                </button>
+                </Button>
               </div>
             </div>
           </div>
@@ -86,7 +93,7 @@ const Votelist = () => {
 
       <br></br>
 
-      <MyBackTop/>
+      <MyBackTop />
 
     </main>
   );
