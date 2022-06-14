c2207cb253eb339d4f479db6bd4a374a5c9bde82
redalygames <67671868+redalygames@users.noreply.github.com>
2022-06-14 10:32:28 +0800

    0614 CSS

--- src/pages/Recipes/Breadcrumb/BreadcrumbCopy.js
+++ src/pages/Recipes/Breadcrumb/BreadcrumbCopy.js
@@ -1,11 +1,12 @@
 import React, { Component, useEffect, useState } from "react";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import "./Breadcrumb.css";
+import "../Recipes/Recipes.css";
 
 class BreadcrumbComponent extends Component {
   render() {
     return (
-      <div className="container col-8" id="break">
+      <div className="container col-8 RecipesOff" id="break">
         <Breadcrumb>
           <Breadcrumb.Item href="/">首頁</Breadcrumb.Item>
           <Breadcrumb.Item ></Breadcrumb.Item>
