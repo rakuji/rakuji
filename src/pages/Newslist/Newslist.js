b2b8bb17cea2e8f4a834c1a7fa76412f0e70161b
WenHsuanWang <yoyoman870221@gmail.com>
2022-06-15 10:43:56 +0800

    Revert "0615"
    This reverts commit 6d8e1f3cbbab8bc28684d2de74101c617ef70f98.

--- src/pages/Newslist/Newslist.js
+++ src/pages/Newslist/Newslist.js
@@ -117,7 +117,7 @@ function Newslist() {
                 <div className="card66">
                   {/* 抓照片  */}
                   <img
-                    src={`${process.env.REACT_APP_API_URL}/img/newslist/${v.imgid}`}
+                    src={`http://localhost/project_rakuji/imgs/${v.imgid}`}
                     className="card-img"
                     alt=""
                   ></img>
